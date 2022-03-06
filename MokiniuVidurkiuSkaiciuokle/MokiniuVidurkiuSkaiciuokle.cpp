79beec51d0a2246db40206eacc958573b06a1d45
NexerG <68482683+NexerG@users.noreply.github.com>
2022-03-06 20:03:40 +0200

    Skaitymas is failo
    Idetas skaitymo is failo funkcionalumas, perdaryta pazimiu rikiavimo funkcija su "sort"

--- MokiniuVidurkiuSkaiciuokle/MokiniuVidurkiuSkaiciuokle.cpp
+++ MokiniuVidurkiuSkaiciuokle/MokiniuVidurkiuSkaiciuokle.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <ctime>
 #include <vector>
+#include <fstream>
+#include <algorithm>
 
 using std::cout;
 using std::cin;
@@ -11,98 +13,158 @@ using std::endl;
 using std::setw;
 using std::fixed;
 using std::setprecision;
-
+using std::left;
+using std::right;
+using std::ifstream;
+using std::ofstream;
+using std::vector;
+using std::noskipws;
+using std::sort;
 
 //constanta nusakanti po kiek pazimiu turi mokiniai
-const int kiek=8;
+int kiek=0;
 
 struct Mokinys
 {
 	string vardas, pavarde;
-	int paz[kiek];
+	vector<int> paz;
 	int egz;				//kintamasis skirtas medianai ir vidurkiui laikyti
 	double rez = 0;
 };
 
-void ivestis(std::vector<Mokinys>& mok, int ciklas);					//duomenu ivedimas
+void ivestis(std::vector<Mokinys>& mok, int ciklas, int ranka, ifstream &fs);			//duomenu ivedimas
 void isvestis(std::vector<Mokinys>& mok, int ciklas);					//isvedimas
 double skaiciavimas(double a, std::vector<Mokinys>& mok, int ciklas);	//galutinio pazimio radimas
 double SkVid(std::vector<Mokinys>& mok, int ciklas);					//vidurkio skaiciavimas
 void rikiavimas(std::vector<Mokinys>& mok, int ciklas);					//rikiavimas
 double SkMed(std::vector<Mokinys>& mok, int ciklas);					//medianos skaiciavimas
+void RikVarPav();
+
 
 int main()
 {
+	int ranka = 0;
 	char Status = 't';			//Statuso kintamasis skirtas suziureti kiek bus mokiniu
 	int ciklas = 0;				//kintamasis kuris nusako kurioje vietoje vektoriaus esame
-	std::vector<Mokinys> Mok;
-	while (Status == 't')		//ciklas skirtas mokiniu funkcijoms atlikti
+	vector<Mokinys> Mok;
+	cout << "ar norite ivesti mokinius ranka (1) ar norite juos paimti is failo (2)?";
+	ifstream fs("kursiokai.txt", ifstream::in);
+	cin >> ranka;
+	if (ranka == 1)
 	{
-		ivestis(Mok,ciklas);
-		rikiavimas(Mok, ciklas);
-		cout << "Dar vieno mokinio vidurkis?[t/n]";
-		cin >> Status;
-		ciklas++;
+		while (Status == 't')		//ciklas skirtas mokiniu funkcijoms atlikti
+		{
+			ivestis(Mok, ciklas,ranka,fs);
+			rikiavimas(Mok, ciklas);
+			cout << "Dar vieno mokinio vidurkis?[t/n]";
+			cin >> Status;
+			ciklas++;
+		}
+	}
+	if (ranka == 2)
+	{
+		while (!fs.eof())
+		{
+			ivestis(Mok, ciklas, ranka, fs);
+			rikiavimas(Mok, ciklas);
+			ciklas++;
+		}
+		fs.close();
+	}
+
+	cout << "rusiuoti mokinius pagal pavardes? ar pagal vardus? (p/v)";
+	char rus;		//rusiavimo kintamasis
+	while (rus != 'p' || rus != 'v')
+	{
+		cin >> rus;
+		if (rus == 'p')
+		{
+			RikVarPav();
+		}
+		else if (rus == 'v')
+		{
+			RikVarPav();
+		}
+		else cout << "ivedete ne toki simboli, veskite is naujo";
 	}
 	isvestis(Mok, ciklas);		//f-ja skirta isvedimui
 }
 
-void ivestis(std::vector<Mokinys>& mok, int ciklas)
+void ivestis(std::vector<Mokinys>& mok, int ciklas, int ranka, ifstream& fs)
 {
-	cout << "Iveskite mokinio varda\n";
 	mok.push_back(Mokinys());
-	cin >> mok[ciklas].vardas;
-	cout << "Iveskite mokinio pavarde\n";
-	cin >> mok[ciklas].pavarde;
+	if (ranka == 1)											//ivestis ranka
+	{
+		cout << "Iveskite mokinio varda\n";
+		mok.push_back(Mokinys());
+		cin >> mok[ciklas].vardas;
+		cout << "Iveskite mokinio pavarde\n";
+		cin >> mok[ciklas].pavarde;
+	}
+
+	if (ranka == 2)											//ivestis is failo
+	{
+		if (ciklas == 0)
+		{
+			fs.ignore(999, '\n');
+		}
+		fs >> mok[ciklas].vardas >> mok[ciklas].pavarde;
+	}
 
 	srand(std::time(NULL));
 
 	//namu darbu pazimiu generavimas
 	char status;
-	cout << "Ar norite ivesti namu darbu pazimi? (t/n)\n";
 	int vieta = 0;
-	while(vieta<kiek)
+	mok[ciklas].paz.push_back(vieta);
+	while (fs >> status)
 	{
-		//cout << "Iveskite namu darbu pazimi\n";
-		mok[ciklas].paz[vieta] = rand() % 10 + 1;
-		//cout << mok[ciklas].paz[vieta] << endl;//debug
-		vieta++;
+		//cout << status << endl;
+		if (status != '\n' && status != ' ' && status <= 57)
+		{
+			if (status != '0')
+			{
+				mok[ciklas].paz.push_back(vieta);
+				mok[ciklas].paz[vieta] = std::stoi(&status);
+				//cout << mok[ciklas].paz[vieta]<<endl;
+				vieta++;
+			}
+			else
+			{
+				mok[ciklas].paz[vieta-1] = mok[ciklas].paz[vieta-1] * 10;
+			}
+		}
+		else if (status > 57) break;
 	}
-
-	//egzamino pazymio generavimas
-	//cout << "Iveskite egzamino pazimi [1;10]\n";
-	mok[ciklas].egz = rand() % 10 + 1;
-	//cout << mok[ciklas].egz << endl;//debug
+	kiek = vieta-1;
+	//cout << endl << vieta;
+	mok[ciklas].egz = mok[ciklas].paz[vieta-1];
 }
 
 void isvestis(std::vector<Mokinys>& mok, int ciklas)
 {
-	cout << "Vardas" << setw(10) << "Pavarde" << setw(20) << "Galutinis (Vid.)" << setw(20) << "Galutinis (Med.)" << endl;
+	cout << "Vardas" << setw(10) << left << "Pavarde" << setw(20) << left << "Galutinis (Vid.)" << setw(20) << left << "Galutinis (Med.)" << endl;
 	cout << "----------------------------------------------------------" << endl;
 	for (int i = 0; i < ciklas; i++)
 	{
-		cout << mok[i].vardas << setw(10) << mok[i].pavarde << setw(15) << fixed << setprecision(2) << SkVid(mok,i) << setw(15) << fixed << setprecision(2) << SkMed(mok, i) << endl;
+		cout << mok[i].vardas << setw(20) << right << mok[i].pavarde << setw(20) << right << fixed << setprecision(2) << SkVid(mok, i) << setw(15) << right << fixed << setprecision(2) << SkMed(mok, i) << endl;
 	}
 }
 
 void rikiavimas(std::vector<Mokinys>& mok, int ciklas)
 {
 	int temp;
-	for (int i = 0; i < kiek; i++)
-		for (int j = i + 1; j < kiek; j++)
-		{
-			if (mok[ciklas].paz[i] > mok[ciklas].paz[j])
-			{
-				temp = mok[ciklas].paz[i];
-				mok[ciklas].paz[i] = mok[ciklas].paz[j];
-				mok[ciklas].paz[j] = temp;
-			}
-		}
+	sort(mok[ciklas].paz.begin(), mok[ciklas].paz.end());
+}
+
+void RikVarPav()
+{
+
 }
 
 double SkVid(std::vector<Mokinys>& mok, int ciklas)
 {
-	double sum = 0, vid;					//pazymiu suma, vidurkis
+	double sum = 0, vid;								//pazymiu suma, vidurkis
 	for (int i = 0; i < kiek; i++)
 	{
 		sum += mok[ciklas].paz[i];
